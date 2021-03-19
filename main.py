8b8b680850a47792261ecb855220503ee2b40dbd
SophiaChen <sophiachen2016@gmail.com>
2021-03-19 09:39:43 -0600

    AddedInCodeBody/Template

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 # Authors: Sophia, Ayushi, Katherine
 
-#
+# Imports:
+
+
+# Global Variables:
+
+
+    # Constants:
+
+
+    # Surface:
+
+
+    # Time:
+
+
+    # Colors:
+
+
+    # Game variables:
+
+
+# Classes:
+
+
+# Non-Class Methods:
+
+def main():
+    pass
+
+# RUN MAIN
+
+if __name__ == '__main__':
+   main()
+
+
+# TESTS
