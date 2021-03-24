d8601eb89395c1864de1cd9bd8b9bbc7bfd6a2fd
SophiaChen <sophiachen2016@gmail.com>
2021-03-24 16:01:35 -0600

    Added Some Outline Stuff

---
+++ cleaningSupplySeed.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+from pygame.locals import *
+
+
+#sprite object that represents the seed packet in your inventory for the game
+
+class CleaningSupplySeed(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 # Authors: Sophia, Ayushi, Katherine
 
 # Imports:
+import pygame
+import sys
+from pygame.locals import *
 
+pygame.init()
 
 # Global Variables:
 
+ # Surface:
+DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    # Constants:
+ # Constants:
+windowWidth = DISPLAYSURF.get_width() # resized to fullscreen
+windowHeight = DISPLAYSURF.get_height() # resized to fullscreen
+
+ # Time:
+FPS = 160
+FPSCLOCK = pygame.time.Clock()
+
+ # Colors:
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+
+ # Game variables:
 
 
-    # Surface:
 
-
-    # Time:
-
-
-    # Colors:
-
-
-    # Game variables:
-
-
-# Classes:
-
+# Classes Are in other .py files
 
 # Non-Class Methods:
+
 
 def main():
     pass
