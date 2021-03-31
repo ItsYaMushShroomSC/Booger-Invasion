ee7c9c6ba4b3ce4acd01abc30842aa0d454475fd
TJKitsinger <odd1out92@gmail.com>
2021-03-31 09:30:47 -0600

    Added CleaningSupply sprite class with some variables and the start for some basic methods.

--- cleaningSupply.py
+++ cleaningSupply.py
@@ -3,6 +3,21 @@ import sys
 from pygame.locals import *
 
 class CleaningSupply(pygame.sprite.Sprite):
+    def __init__(self, row, column, type, price, img):
+        self.x = row
+        self.y = column
+        self.type = type # string name
+        self.price = price
+        self.destroyed = False
+        self.image = img
+        self.rect = self.image.getRect()
 
-    def __init__(self):
-        pass
+    #this will be used to activate the cleaning supply's ability, whether it is attacking bugs, healing other plants, producing money, etc.
+    def activate(self):
+        print("cleaningsupply activated")
+        return
+
+    #this will be used to keep track of the cleaningsupply's health when it is attacked by bugs.
+    def attacked(self):
+        print("cleaningsupply attacked")
+        return
