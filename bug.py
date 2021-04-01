93ea066da87113a862ac952a4d84082a1d31af17
Ayushi <ayushimohanty2015@gmail.com>
2021-04-01 13:05:51 -0600

    spider spawning

--- bug.py
+++ bug.py
@@ -2,22 +2,21 @@ import pygame
 import sys
 from pygame.locals import *
 from main import *
+# spider should take 5 hits before its death
 
 
 all_sprites = pygame.sprite.Group()
 
 class Bug(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
-        # self.image = pygame.image.load("spider.png")
-        # self.rect = self.image.get_rect()
-        # self.rect.center = (200,200)
+        x,y = pos
         self.image = pygame.Surface((50, 50))
         self.image = pygame.transform.scale(pygame.image.load("spider.png"), (100,100))
 
         self.rect = self.image.get_rect()
-        self.rect.center = (DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2)
+        self.rect.center = (x,y)
 
 
     def update(self):
@@ -26,5 +25,14 @@ class Bug(pygame.sprite.Sprite):
             self.rect.right = 0
 
 
-bug1 = Bug()
+bug1 = Bug((DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2))
+bug2 = Bug((DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2 - 121))
+bug3 = Bug((DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2 - 242))
+bug4 = Bug((DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2 + 121))
+bug5 = Bug((DISPLAYSURF.get_width() / 2, DISPLAYSURF.get_height() / 2 + 242))
 all_sprites.add(bug1)
+all_sprites.add(bug2)
+all_sprites.add(bug3)
+all_sprites.add(bug4)
+all_sprites.add(bug5)
+
--- cleaningSupply.py
+++ cleaningSupply.py
@@ -2,22 +2,26 @@ import pygame
 import sys
 from pygame.locals import *
 
+
 class CleaningSupply(pygame.sprite.Sprite):
-    def __init__(self, row, column, type, price, img):
-        self.x = row
+
+    def __init__(self, row, column, type, price, supplyhealth, cooldown, img):
+        self.x = row  # the row and column are in the game board grid, not the pixel x coordinate
         self.y = column
-        self.type = type # string name
+        self.type = type  # string name
         self.price = price
         self.destroyed = False
         self.image = img
         self.rect = self.image.getRect()
+        self.cooldown = cooldown  # how long between the supply being able to use its abilities.
+        self.elapsedCooldown
+        self.health = health  # the amount of times the supply can be attacked before it is destroyed.
 
-    #this will be used to activate the cleaning supply's ability, whether it is attacking bugs, healing other plants, producing money, etc.
+    # this will be used to activate the cleaning supply's ability, whether it is attacking bugs, healing other plants, producing money, etc.
     def activate(self):
-        print("cleaningsupply activated")
-        return
+        if (self.elapsedCooldown == 0):
+            return
 
-    #this will be used to keep track of the cleaningsupply's health when it is attacked by bugs.
+    # this will be used to keep track of the cleaningsupply's health when it is attacked by bugs.
     def attacked(self):
-        print("cleaningsupply attacked")
-        return
+        self.supplyhealth -= 1
