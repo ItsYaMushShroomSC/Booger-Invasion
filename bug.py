0f2be77bdd7de958c89f493eb0ebbad35acb64aa
Ayushi <ayushimohanty2015@gmail.com>
2021-04-06 13:04:47 -0600

    bug death and limited movement added

--- bug.py
+++ bug.py
@@ -16,7 +16,7 @@ class Spider(pygame.sprite.Sprite):
         scaleFactor = scaleFactorH
         if scaleFactorW < scaleFactorH:
             scaleFactor = scaleFactorW
-
+        self.health = 5
         self.image = pygame.transform.scale(pygame.image.load("spider.png"), (100 * scaleFactor, 121 * scaleFactor))
 
         self.rect = self.image.get_rect()
@@ -26,44 +26,56 @@ class Spider(pygame.sprite.Sprite):
 
 
     def update(self):
+        self.rect.x -= 10
+        if (self.rect.x <= (DISPLAYSURF.get_width())/4):
+            self.rect.x += 10
+            self.health = 0
+        self.die()
 
-        self.rect.x -= 20
-        if self.rect.left > DISPLAYSURF.get_width():
-            self.rect.x = 0
-
+    def die(self):
+        if self.health == 0:
+            self.kill()
 
 
 class Cockroach(pygame.sprite.Sprite):
 
-    def __init__(self, pos):
+    def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-        x,y = pos
-        self.image = pygame.Surface((50, 50))
-        self.image = pygame.transform.scale(pygame.image.load("cockroach.png"), (100,100))
+        self.x = x
+        self.y = y
+        scaleFactor = scaleFactorH
+        if scaleFactorW < scaleFactorH:
+            scaleFactor = scaleFactorW
+
+        self.image = pygame.transform.scale(pygame.image.load("cockroach.png"), (100 * scaleFactor, 121 * scaleFactor))
 
         self.rect = self.image.get_rect()
-        self.rect.center = (x,y)
-
+        self.rect.center = (x, y)
 
     def update(self):
-        self.rect.x += 5
-        if self.rect.left > DISPLAYSURF.get_width():
-            self.rect.right = 0
+        self.rect.x -= 10
+        if (self.rect.x <= (DISPLAYSURF.get_width()) / 4):
+            self.rect.x += 10
+            self.health = 0
+        self.die()
+
+    def die(self):
+        if self.health == 0:
+            self.kill()
 
 
 
-def death(self):
-
-    if self.x == DISPLAYSURF.get_width() / 4 and self.y == DISPLAYSURF.get_height() / 2:
-        enemy_sprites.remove(self)
 
 bug1 = Spider(3*DISPLAYSURF.get_width() / 4, DISPLAYSURF.get_height() / 2)
 bug2 = Spider(3*DISPLAYSURF.get_width() / 4, DISPLAYSURF.get_height() / 2 - 121)
 bug3 = Spider(3*DISPLAYSURF.get_width() / 4, DISPLAYSURF.get_height() / 2 - 242)
 bug4 = Spider(3*DISPLAYSURF.get_width() / 4, DISPLAYSURF.get_height() / 2 + 121)
 bug5 = Spider(3*DISPLAYSURF.get_width() / 4, DISPLAYSURF.get_height() / 2 + 242)
+cock1 = Cockroach(3*DISPLAYSURF.get_width() / 4 + 50, DISPLAYSURF.get_height() / 2 + 242)
+
 enemy_sprites.add(bug1)
 enemy_sprites.add(bug2)
 enemy_sprites.add(bug3)
 enemy_sprites.add(bug4)
 enemy_sprites.add(bug5)
+enemy_sprites.add(cock1)
