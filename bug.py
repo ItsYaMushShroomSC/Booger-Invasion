8802dd24aa26668268114fce76e6857e8ccc0ae2
677626656 <sophiachen2016@gmail.com>
2021-05-11 09:59:00 -0600

    worked on bleach implementing

--- bug.py
+++ bug.py
@@ -15,14 +15,15 @@ YMARG = defaults.YMARGIN
 
 class Spider(Bug):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, row):
         img = pygame.image.load('spider.png')
-        super().__init__(x, y, img, False) # check out supremeBug.py to see what this initializes
-
+        super().__init__(x, y, row, img, False) # check out supremeBug.py to see what this initializes
+        self.name = 'spider'
         self.health = 5
-        self.defaultSpeed = 10
-        self.speed = 10
+        self.speed = 8
+        self.defaultSpeed = 8
         self.damage = 1
+        self.row = row
 
     def update(self):
         super().update(self.speed)
@@ -31,23 +32,20 @@ class Spider(Bug):
         cleaningSupply.health -= self.damage
 
     def moveBack(self):
-        super().moveBack(self.defaultSpeed)
-
-
-
-
+        super().moveBack(self.defaultSpeed, False)
 
 class Cockroach(Bug):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, row):
 
         img = pygame.image.load('cockroach.png')
-        super().__init__(x, y, img, False)  # check out supremeBug.py to see what this initializes
-
+        super().__init__(x, y, row, img, False)  # check out supremeBug.py to see what this initializes
+        self.name = 'cochroach'
         self.health = 7
         self.speed = 10
         self.defaultSpeed = 10
         self.damage = 1
+        self.row = row
 
     def update(self):
         super().update(self.speed)
@@ -56,19 +54,20 @@ class Cockroach(Bug):
         cleaningSupply.health -= self.damage
 
     def moveBack(self):
-        super().moveBack(self.defaultSpeed)
+        super().moveBack(self.defaultSpeed, False)
 
 class LadyBug(Bug):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, row):
 
         img = pygame.image.load('ladybug.png')
-        super().__init__(x, y, img, False)  # check out supremeBug.py to see what this initializes
-
-        self.health = 9
-        self.speed = 10
-        self.defaultSpeed = 10
+        super().__init__(x, y, row, img, False)  # check out supremeBug.py to see what this initializes
+        self.name = 'ladybug'
+        self.health = 6
+        self.speed = 9
+        self.defaultSpeed = 9
         self.damage = 1
+        self.row = row
 
     def update(self):
         super().update(self.speed)
@@ -77,19 +76,20 @@ class LadyBug(Bug):
         cleaningSupply.health -= self.damage
 
     def moveBack(self):
-        super().moveBack(self.defaultSpeed)
+        super().moveBack(self.defaultSpeed, False)
 
 class Wasp(Bug):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, row):
 
         img = pygame.image.load('wasp.png')
-        super().__init__(x, y, img, True)  # check out supremeBug.py to see what this initializes
-
-        self.health = 11
-        self.speed = 10
-        self.defaultSpeed = 10
+        super().__init__(x, y, row, img, False)  # check out supremeBug.py to see what this initializes
+        self.name = 'wasp'
+        self.health = 10
+        self.speed = 8
+        self.defaultSpeed = 8
         self.damage = 1
+        self.row = row
 
     def update(self):
         super().update(self.speed)
@@ -98,19 +98,20 @@ class Wasp(Bug):
         cleaningSupply.health -= self.damage
 
     def moveBack(self):
-        super().moveBack(self.defaultSpeed)
+        super().moveBack(self.defaultSpeed, False)
 
 class Ant(Bug):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, row):
 
-        img = pygame.image.load('ant1.png')
-        super().__init__(x, y, img, False)  # check out supremeBug.py to see what this initializes
-
-        self.health = 13
+        img = pygame.image.load('ant.png')
+        super().__init__(x, y, row, img, False)  # check out supremeBug.py to see what this initializes
+        self.name = 'ant'
+        self.health = 6
         self.speed = 10
         self.defaultSpeed = 10
         self.damage = 1
+        self.row = row
 
     def update(self):
         super().update(self.speed)
@@ -119,12 +120,13 @@ class Ant(Bug):
         cleaningSupply.health -= self.damage
 
     def moveBack(self):
-        super().moveBack(self.defaultSpeed)
+        super().moveBack(self.defaultSpeed, False)
 
 class GiantBug(Bug):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, row):
         pygame.sprite.Sprite.__init__(self)
+        self.name = 'giantbug'
         self.x = x
         self.y = y
         scaleFactor = defaults.scaleFactorH
@@ -133,10 +135,11 @@ class GiantBug(Bug):
         self.image = pygame.transform.scale(pygame.image.load("ant (1).png"), (400,600))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.health = 13
+        self.health = 100
         self.speed = 6
-        self.defaultSpeed = 10
+        self.defaultSpeed = 6
         self.damage = 1
+        self.row = row
 
     def update(self):
         super().update(self.speed)
@@ -145,4 +148,4 @@ class GiantBug(Bug):
         cleaningSupply.health -= self.damage
 
     def moveBack(self):
-        super().moveBack(self.defaultSpeed)
+        super().moveBack(self.defaultSpeed, True)
