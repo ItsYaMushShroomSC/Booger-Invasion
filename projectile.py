a77710359f90aa3774d31105c0941d50273053ef
677626656 <sophiachen2016@gmail.com>
2021-05-12 09:57:36 -0600

    tweaked game enter times and stuff

--- projectile.py
+++ projectile.py
@@ -15,16 +15,20 @@ class Bullet(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         scaleFactor = scaleFactorH
+        self.damage = 1
+
         if scaleFactorW < scaleFactorH:
             scaleFactor = scaleFactorW
         self.health = 5
         if(type == 'spraydroplet'):
             self.image = pygame.transform.scale(pygame.image.load("waterdroplet.PNG"), (50, 50))
             self.type = "spraydroplet"
+            self.damage = 1.5
         #self.image = pygame.transform.rotate(self.image, 90)
         if(type == 'icedroplet'):
             self.type = "icedroplet"
             self.image = pygame.transform.scale(pygame.image.load("icedroplet.png"), (50, 50))
+            self.damage = 2
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
 
